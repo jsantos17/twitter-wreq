1140f140db9198172d707a36c79d709132a533e7
Juan Pablo Santos <jsantos@stackbuilders.com>
2014-12-28 21:02:30 -0500

    Implement temp token acquisition

--- src/Web/TwitterClient.hs
+++ src/Web/TwitterClient.hs
@@ -1,14 +1,33 @@
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Web.TwitterClient (acquireTempTokens, acquireAccessTokens, displayTimeline) where
 
+import Data.ByteString.Char8 (pack)
+import Data.ByteString.Lazy (ByteString(..), toStrict)
+import Control.Applicative
 import Control.Lens
 import Network.Wreq
+import Network.HTTP.Types.URI
 import Web.TwitterClient.Types
 
 
+findTokensInResponse :: ByteString -> Maybe TokenPair
+findTokensInResponse response = do
+  token       <- lookup "oauth_token" body
+  tokenSecret <- lookup "oauth_token_secret" body
+  return $ TokenPair token tokenSecret
+    where body = parseQuery (toStrict response)
+
 acquireTempTokens :: APIKeys -> CallbackURI -> IO TokenPair
-acquireTempTokens APIKeys{..} callback = undefined
+acquireTempTokens APIKeys{..} callback = do
+  r <- postWith opts "https://api.twitter.com/oauth/authenticate" ("" :: ByteString)
+  case (r ^? responseBody) of
+    Just response -> case findTokensInResponse response of
+                       Just tokens -> return tokens
+                       Nothing     -> fail "Couldn't find tokens in response from Twitter"
+    Nothing       -> fail "Twitter replied with an empty body"
+  where opts = defaults & auth ?~ oauth1Temp (pack consumerKey) (pack consumerSecret) (pack callback)
 
 acquireAccessTokens :: APIKeys -> TokenPair -> OAuthVerifier -> IO TokenPair
 acquireAccessTokens = undefined
